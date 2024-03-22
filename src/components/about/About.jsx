1b71d80df7d082b8b0a684ce2877bef105a45f1f
Bilostenko <bilostenkod@gmail.com>
2024-03-22 14:51:16 +0200

    update About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,9 @@
 import './about.css';
-
-import personalPhoto from '../../assets/image/hero_photo.jfif';
+import {herodescription} from '../../data/data'
 
 const About = () => {
+  const herodescriptionData = herodescription();
+
   return (
     <section className="about">
       <div className="container">
@@ -10,18 +11,17 @@ const About = () => {
         <div className="about__wrapper">
           <div className="about__description">
             <ul className="about__description-list">
-              <li className="about__desctiption-item">I have confirmed  CSS skills on LinkedIn </li>
-              <li className="about__desctiption-item">Certification from Freecodecamp course on "Responsive Web Design" and "JavaScript Algorithms and Data Structures", certification of completion of CyberBionic systematics school and IT Generation</li>
-              <li className="about__desctiption-item">4 kyu Rank on CodeWars with total completed 600+ challenges in JavaScript</li>
+              <li className="about__desctiption-item">{herodescriptionData.achievementList.item1}</li>
+              <li className="about__desctiption-item">{herodescriptionData.achievementList.item2}</li>
+              <li className="about__desctiption-item">{herodescriptionData.achievementList.item3}</li>
             </ul>
           </div>
           <div className="about__photo">
-            <img src={personalPhoto} alt="personal photo" />
+            <img src={herodescriptionData.personalPhotoImg} alt="persona" />
           </div>
         </div>
       </div>
     </section>
-// ВИРІШИТИ ПИТАННЯ ЗІ ЗМЕНШУВАНИМ ФОТО
   );
 }
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import workplace from '../assets/image/hero-workplace.jpg';
+import personalPhoto from '../assets/image/hero_photo.jfif';
 
 import instagram from '../assets/icons/free-icon-instagram-1384089.png';
 import linkedin from '../assets/icons/free-icon-linkedin-618.png';
@@ -11,25 +12,29 @@ import facebook from '../assets/icons/free-icon-facebook-5968764.png';
  */
 export const herodescription = () => {
   const workPlaceImg = workplace;
+  const personalPhotoImg = personalPhoto;
   const name = 'Dima BILOSTENKO';
   const description = (
     <p className="hero__social-personal">I`m hard working <strong className='text-white'>software developer</strong>  who looks for working with open minded team of individuals</p>
   );
+  const achievementList = { item1: "I have confirmed  CSS skills on LinkedIn", item2: "Certification from Freecodecamp course on `Responsive Web Design` and `JavaScript Algorithms and Data Structures`, certification of completion of CyberBionic systematics school and IT Generation", item3: "4 kyu Rank on CodeWars with total completed 600+ challenges in JavaScript" }
 
   return {
     workPlaceImg,
     name,
-    description
+    description,
+    achievementList,
+    personalPhotoImg
   };
 };
 
 export const social = () => {
-  const inst = {img:instagram, link:"https://www.instagram.com/di_sykes9/"};
-  const lnkdn = {img:linkedin, link:"https://www.linkedin.com/in/bilostenko/"} ;
-  const gitHb = {img:gitHub, link:"https://github.com/Bilostenko"};
-  const fb = {img:facebook, link:"https://www.facebook.com/bilostenkod"};
+  const inst = { img: instagram, link: "https://www.instagram.com/di_sykes9/" };
+  const lnkdn = { img: linkedin, link: "https://www.linkedin.com/in/bilostenko/" };
+  const gitHb = { img: gitHub, link: "https://github.com/Bilostenko" };
+  const fb = { img: facebook, link: "https://www.facebook.com/bilostenkod" };
 
-  return{
+  return {
     inst,
     lnkdn,
     gitHb,
