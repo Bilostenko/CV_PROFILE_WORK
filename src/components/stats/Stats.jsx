031417d06114d49715087985a51360d8e2c40fef
Bilostenko <bilostenkod@gmail.com>
2024-04-01 17:57:24 +0300

    add API Codewars

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,27 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import './stats.css';
 import { stats } from '../../data/data';
-// import GitHubRepositoriesCounter from '../../services/GitHubService';
+import useApiService from '../../services/APIservice';
 
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
 const Stats = () => {
 
   const statsData = stats();
-  // const [repositoriesCount, setRepositoriesCount] = useState(null);
+  const { error, loading, getRepoCount, getCodeWarsCount, clearError } = useApiService()
 
-  // useEffect(() => {
-  //   const fetchRepositoriesCount = async () => {
-  //     try {
-  //       const response = await GitHubRepositoriesCounter();
-  //       setRepositoriesCount(response);
-  //     } catch (error) {
-  //       console.error('Помилка при отриманні кількості репозиторіїв:', error);
-  //     }
-  //   };
+  const [gitHubCount, setgitHubCount] = useState(null);
+  const [codewarsCount, setcodewarsCount] = useState(null);
 
-  //   fetchRepositoriesCount();
-  // }, []);
+  useEffect(() => {
+    updateGitHubRepos()
+  }, [])
 
-  // ДОДАТИ СПІННЕР 
+  const updateGitHubRepos = () => {
+    clearError()
+
+    getRepoCount()
+      .then(res => {
+        setgitHubCount(res);
+      })
+      .catch(err => {
+        console.error('Error while fetching data:', err);
+      });
+
+    getCodeWarsCount()
+      .then(res => {
+        setcodewarsCount(res);
+      })
+      .catch(err => {
+        console.error('Error while fetching data:', err);
+      });
+  }
+
+
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
 
   return (
     <div className="container">
@@ -33,14 +51,23 @@ const Stats = () => {
             <img src={statsData.gitHubStats} alt="logo" />
           </div>
           <h3 className="stats__number">Repos on GitHub</h3>
-          {/* <p className="stats__text">{repositoriesCount === null ? 'Loading...' : repositoriesCount}</p> */}
+          <p className="stats__text">
+            {errorMessage}
+            {spinner}
+            {gitHubCount ? gitHubCount.length : 'Loading...'}
+          </p>
         </div>
         <div className="stats__item">
           <div className="stats__item-logo">
             <img src={statsData.codeWarsStats} alt="logo" />
           </div>
           <h3 className="stats__number">Completed tasks on Codewars</h3>
-          <p className="stats__text">11</p>
+          <p className="stats__text">
+            {errorMessage}
+            {spinner}
+            {typeof codewarsCount === 'number' ? codewarsCount : 'Loading...'}
+            {/* НЕ ПРЦЮЄ */}
+          </p>
         </div>
         <div className="stats__item">
           <div className="stats__item-logo">
