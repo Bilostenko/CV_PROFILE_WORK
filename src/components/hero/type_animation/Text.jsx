1f52731855c4a7c7da6d405a1dbc94fde81e5340
Bilostenko <bilostenkod@gmail.com>
2024-06-10 17:08:04 +0300

    update text typing animation

--- src/components/hero/type_animation/Text.jsx
+++ src/components/hero/type_animation/Text.jsx
@@ -1,35 +1,30 @@
-import Typist from 'react-typist';
 import React, { useState } from 'react';
+import { Typewriter } from 'react-simple-typewriter';
 
-import './text.css'
+import './text.css';
 
 const Text = () => {
-
   const [loopCount, setLoopCount] = useState(0);
 
   const handleTypistLoop = () => {
     setLoopCount(loopCount + 1);
   };
 
-
-  return ( 
+  return (
     <div className="typist">
-       <Typist avgTypingDelay={80} cursor={{ show: false }} onTypingDone={handleTypistLoop} key={loopCount}>
-                <span>HTML</span>
-                <Typist.Backspace count={4} delay={700} />
-                <span>CSS</span>
-                <Typist.Backspace count={3} delay={800} />
-                <span>JavaScript</span>
-                <Typist.Backspace count={10} delay={900} />
-                <span>React</span>
-                <Typist.Backspace count={5} delay={1000} />
-                <span>Redux</span>
-                <Typist.Backspace count={5} delay={1100} />
-                <span className="many-more"> more oher stack...</span>
-                <Typist.Backspace count={16} delay={1200} />
-              </Typist>
+      <Typewriter
+        loop
+        words={['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'more other stack...']}
+        cursor
+        cursorStyle="_"
+        typeSpeed={80}
+        deleteSpeed={50}
+        delaySpeed={700}
+        onLoopDone={handleTypistLoop}
+        key={loopCount}
+      />
     </div>
-   );
-}
- 
+  );
+};
+
 export default Text;
